351649e5b1eac81b4938d701484934d2c476a55c
Arthur Junior <arthurjr1906@gmail.com>
2023-11-14 22:29:34 -0300

    feat: add login and register form on main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import { LoginRegisterForm } from '@/components'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Main page</h1>
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <LoginRegisterForm />
     </main>
   )
 }
