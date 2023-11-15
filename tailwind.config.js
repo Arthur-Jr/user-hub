3945c73703b1dabfe9fca8bda8ca011afa02493b
Arthur Junior <arthurjr1906@gmail.com>
2023-11-14 22:28:32 -0300

    feat: add primary color on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'primary-color': 'var(--primary-color)'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
