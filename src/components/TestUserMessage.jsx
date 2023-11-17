6012f2057877b4c549dfcf93ff50dd8f73fe5209
Arthur Junior <arthurjr1906@gmail.com>
2023-11-16 22:50:00 -0300

    feat: add test user msg

---
+++ src/components/TestUserMessage.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+function TestUserMessage() {
+  return (
+    <div className="flex justify-center text-center w-full bg-red-500 p-4">
+      <span className="text-xl font-bold italic">
+        Your account is a test account, <Link className="underline underline-offset-2 hover:text-white" href="/test-user">Register an email</Link> to keep this account!
+      </span>
+    </div>
+  )
+}
+
+export default TestUserMessage
