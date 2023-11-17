927d4812d5e326e6113b757f3a1165358edb8659
Arthur Junior <arthurjr1906@gmail.com>
2023-11-17 16:45:52 -0300

    feat: add main msg on projects page

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -5,8 +5,8 @@ function Projects() {
     <main>
       <Header />
 
-      <div className="h-[100vh]">
-        Projects
+      <div className="h-[100vh] px-10 w-full">
+        <h2 className="text-center text-xl font-semibold italic">Projects that can be accessed with this account</h2>
       </div>
     </main>
   )
