5c33a1b143fb4669bf7b9918afb605322b5f8754
Arthur Junior <arthurjr1906@gmail.com>
2023-11-17 18:38:45 -0300

    fix: fix endpoint

--- src/components/TestUserMessage.jsx
+++ src/components/TestUserMessage.jsx
@@ -4,7 +4,7 @@ function TestUserMessage() {
   return (
     <div className="flex justify-center text-center w-full bg-red-500 p-4">
       <span className="text-xl font-bold italic">
-        Your account is a test account, <Link className="underline underline-offset-2 hover:text-white" href="/test-user">Register an email</Link> to keep this account!
+        Your account is a test account, <Link className="underline underline-offset-2 hover:text-white" href="/add-email">Register an email</Link> to keep this account!
       </span>
     </div>
   )
