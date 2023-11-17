68087fa90e59af329c623d4819a596dac8129c91
Arthur Junior <arthurjr1906@gmail.com>
2023-11-17 16:55:46 -0300

    feat: add loading component

---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-[100vh] w-full">
+      <span className="font-bold text-lg text-center text-primary-color">Loading...</span>
+    </div>
+  )
+}
