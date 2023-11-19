610a9506f6af9b50f37673af4cf6aeebd71427f9
Arthur Junior <arthurjr1906@gmail.com>
2023-11-19 01:40:33 -0300

    feat: implement endpoint var on testUserMsg

--- src/components/TestUserMessage.jsx
+++ src/components/TestUserMessage.jsx
@@ -1,10 +1,11 @@
+import endpoints from '@/constants/endpoints';
 import Link from 'next/link';
 
 function TestUserMessage() {
   return (
     <div className="flex justify-center text-center w-full bg-red-500 p-4">
       <span className="text-xl font-bold italic">
-        Your account is a test account, <Link className="underline underline-offset-2 hover:text-white" href="/add-email">Register an email</Link> to keep this account!
+        Your account is a test account, <Link className="underline underline-offset-2 hover:text-white" href={ endpoints.addEmail }>Register an email</Link> to keep this account!
       </span>
     </div>
   )
