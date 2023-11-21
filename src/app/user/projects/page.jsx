4ee92e68a453d30f9541026b0c4c4b8194542ed1
Arthur Junior <arthurjr1906@gmail.com>
2023-11-21 18:21:05 -0300

    feat: add loading svg

--- src/app/user/projects/page.jsx
+++ src/app/user/projects/page.jsx
@@ -1,7 +1,7 @@
 function Projects() {
   return (
     <main>
-      <div className="h-[100vh] px-10 w-full">
+      <div className="px-10 w-full">
         <h2 className="text-center text-xl font-semibold italic">Projects that can be accessed with this account</h2>
       </div>
     </main>
