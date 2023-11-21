0ba969c1f14badf7dae13a6dbd88d970305f3c39
Arthur Junior <arthurjr1906@gmail.com>
2023-11-20 22:10:57 -0300

    feat: add token validation on reset password page

--- src/app/reset/[token]/page.jsx
+++ src/app/reset/[token]/page.jsx
@@ -3,8 +3,11 @@
 import Form from "@/components/Form";
 import { useEffect, useState } from "react";
 import { useRouter } from 'next/navigation';
+import { jwtDecode } from 'jwt-decode';
 import endpoints from "@/constants/endpoints";
 import getUserData from "@/requests/getUserData";
+import resetPassword from "@/requests/resetPassword";
+import constants from "@/constants/data";
 
 function ResetPassword({ params: { token } }) {
   const [responseMsg, setResponseMsg] = useState('');
@@ -13,26 +16,53 @@ function ResetPassword({ params: { token } }) {
 
   useEffect(() => {
     if (!token) {
+      localStorage.removeItem(constants.localStorageTokenName);
+      router.push(endpoints.home);
+      return;
+    }
+
+    const { data } = jwtDecode(token);
+    if (!data.reset) {
+      localStorage.removeItem(constants.localStorageTokenName);
       router.push(endpoints.home);
       return;
     }
 
     getUserData(token).then((result) => {
       if (!result.username) {
+        localStorage.removeItem(constants.localStorageTokenName);
         router.push(endpoints.home);
       }
     });
   }, [router, token]);
 
-  const handleSubmit = (e, userData) => {
+  const handleSubmit = async (e, userData) => {
     e.preventDefault();
+
+    const result = await resetPassword(token, userData);
+
+    if (result.status === 204) {
+      setResponseMsg(result.message);
+
+      setTimeout(() => {
+        router.push(endpoints.home);
+      }, 6000);
+    }
+
+    if (result.status === 500) {
+      setResponseMsg(result.message);
+    }
+
+    if (result.status === 401) {
+      router.push(endpoints.home);
+    }
   }
 
   return (
-    <main className="h-[100vh] flex flex-col items-center w-[100%] p-10">
+    <main className="flex flex-col items-center w-[100%] p-10">
       <h1 className="text-center text-3xl font-bold italic mb-5">Reset Password</h1>
 
-      <div className="flex flex-col items-center w-[400px] h-[450px]">
+      <div className="flex flex-col items-center w-[400px] h-[550px]">
         <Form fields={ formFields } setResponseMsg={ setResponseMsg } handleSubmit={ handleSubmit } page="reset" />
 
         { responseMsg.length > 0 && <span className="text-lg italic font-bold text-black text-center">{ responseMsg }</span> }
