a959d50deb1c16efdb2dd4bc65bd5a43cf038d01
Arthur Junior <arthurjr1906@gmail.com>
2023-11-21 18:08:15 -0300

    feat: add provider on user layout

--- src/app/user/layout.js
+++ src/app/user/layout.js
@@ -1,10 +1,11 @@
 import { Header } from '@/components';
+import AppProvider from '@/context/AppProvider';
 
 export default function UserLayout({ children }) {
   return (
-    <>
+    <AppProvider>
       <Header />
       {children}
-    </>
+    </AppProvider>
   )
 };
