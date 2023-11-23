4bd1fdc456316a560eaba40b577f8342b86f564b
Arthur Junior <arthurjr1906@gmail.com>
2023-11-23 20:05:31 -0300

    fix: exchange forms from add-email page

--- src/app/user/add-email/page.jsx
+++ src/app/user/add-email/page.jsx
@@ -1,19 +1,23 @@
 'use client';
 
-import { useContext, useEffect, useState } from "react";
-import { useRouter } from 'next/navigation';
-import { jwtDecode } from "jwt-decode";
+import { FormButton } from "@/components/Form/FormButton";
+import { FormField } from "@/components/Form/FormField";
+import { FormInput } from "@/components/Form/FormInput";
+import { FormLabel } from "@/components/Form/FormLabel";
 import constants from "@/constants/data";
-import addEmail from "@/requests/addEmail";
-import Form from "@/components/Form";
 import endpoints from "@/constants/endpoints";
 import { appContext } from "@/context/AppProvider";
+import addEmail from "@/requests/addEmail";
+import { jwtDecode } from "jwt-decode";
+import { useRouter } from 'next/navigation';
+import { useContext, useEffect, useState } from "react";
+import { FormProvider, useForm } from 'react-hook-form';
 
 function AddEmail() {
-  const router = useRouter();
   const [responseMsg, setResponseMsg] = useState('');
   const { userData } = useContext(appContext);
-  const formFields = ['email', 'password']
+  const router = useRouter();
+  const methods = useForm();
 
   useEffect(() => {
     const token = localStorage.getItem(constants.localStorageTokenName);
@@ -29,11 +33,9 @@ function AddEmail() {
     }
   }, [router]);
 
-  const handleSubmit = async (e, formData) => {
-    e.preventDefault();
-
+  const addNewEmail = async (userData) => {
     const token = localStorage.getItem(constants.localStorageTokenName);
-    const result = await addEmail(token, formData);
+    const result = await addEmail(token, userData);
 
     if (result.token) {
       localStorage.setItem(constants.localStorageTokenName, result.token);
@@ -44,12 +46,28 @@ function AddEmail() {
   }
 
   return (
-    <main className="w-full flex flex-col items-center">
-      { userData.username.length > 0 &&
+    <main className="w-full flex flex-col items-center h-96">
+      { userData.username.length === 0 &&
         <div className="w-full sm:w-[650px] flex flex-col items-center">
           <h1 className="text-center text-xl font-bold italic mb-5">Add an email to your test account!</h1>
 
-          <Form fields={ formFields } handleSubmit={ handleSubmit } setResponseMsg={ setResponseMsg } page="add email"/>
+          <FormProvider { ...methods }>
+            <form className="flex flex-col items-center justify-around w-full gap-8 p-10" onSubmit={ methods.handleSubmit(addNewEmail) }>
+              <FormField>
+                <FormLabel htmlFor="email">email</FormLabel>
+                <FormInput type="text" name="email" minLength="6" maxLength="30" required/>
+              </FormField>
+
+              <FormField>
+                <FormLabel htmlFor="password">password</FormLabel>
+                <FormInput type="password" name="password" required minLength="6" maxLength="16"/>
+              </FormField>
+
+              <FormButton type="submit">
+                Register
+              </FormButton>
+            </form>
+          </FormProvider>
 
           { responseMsg.length > 0 && <span className="text-lg italic font-bold text-black text-center">{ responseMsg }</span> }
         </div>
