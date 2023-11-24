1d94b1aa4e1e97002815508b0cfdf7e813a92612
Arthur Junior <arthurjr1906@gmail.com>
2023-11-24 17:49:52 -0300

    fix: fix account form css

--- src/components/AccountForm.jsx
+++ src/components/AccountForm.jsx
@@ -13,10 +13,9 @@ export default function AccountForm({ editUser, userData }) {
   const [isEmailDisabled, setIsEmailDisabled] = useState(true);
   const methods = useForm({ defaultValues: { username: userData.username, email: userData.email } });
 
-  const fieldsCss = 'flex flex-col md:grid grid-cols-2 items-start justify-between w-[300px] md:w-[80%] lg:w-[70%]';
-  const inputCss = 'text-black font-semibold text-base italic p-2 rounded-md w-full border-2 border-black disabled:bg-gray-500 disabled:cursor-not-allowed';
-  const emailBtnCss = 'border-2 border-black rounded-md hover:scale-105 ml-1 w-[45px] h-[42px] flex items-center justify-center text-red-700 text-2xl disabled:cursor-not-allowed';
-  const labelCss = 'flex flex-col items-start text-xl font-bold italic uppercase w-[75%] text-center';
+  const fieldsCss = 'md:grid grid-cols-2 items-start w-[300px] md:w-[80%] lg:w-[70%]';
+  const inputCss = 'font-semibold text-base w-full disabled:bg-gray-500 disabled:cursor-not-allowed';
+  const labelCss = 'items-start w-[75%]';
 
   const handleEditEmailBtn = () => {
     if (isEmailDisabled) {
@@ -45,8 +44,17 @@ export default function AccountForm({ editUser, userData }) {
             <div className="flex items-center w-full">
               <FormInput type="text" name="email" minLength="6" maxLength="30" disabled={ isEmailDisabled } className={ inputCss }/>
 
-              <FormButton type="button" className={ emailBtnCss } onClick={ () => handleEditEmailBtn() }>
-                <Image src={ editImg } alt="Enable edit email button" width={25} height={25}/>
+              <FormButton
+                type="button"
+                onClick={ () => handleEditEmailBtn() }
+                className="ml-1 w-[45px] h-[43px] text-red-700 text-xl p-1 mt-2 flex items-center justify-center disabled:bg-gray-500 disabled:cursor-not-allowed"
+                disabled={ !userData.status }
+                title={ isEmailDisabled ? 'Edit email' : 'Cancel email edit' }
+              >
+                { isEmailDisabled ? 
+                  <Image src={ editImg } alt="Enable edit email button" width={25} height={25}/> :
+                  'X'
+                }
               </FormButton>
             </div>
           </FormField>
